596dc1e43d04ed2905ff90e92d85d62b493e1d8b
Frank LENORMAND <lenormf@gmail.com>
2016-01-06 10:35:16 +0200

    Implement a backup functionality

--- js/database.js
+++ js/database.js
@@ -13,7 +13,7 @@
                 return null;
             }
 
-            var ret = window.localStorage.getItem(key);
+            const ret = window.localStorage.getItem(key);
             return (ret !== null) ? JSON.parse(ret) : null;
         },
         setItem: function (key, value) {
@@ -24,6 +24,32 @@
     };
 
     const PREFIX_DB = "_health_";
+    const VERSION_DB = 1.0;
+
+    // Keep track of all the dates on which items were added
+    // Instead of fetching the list of all items that are in database everytime we want to add an item,
+    // just save the date in a smaller array to allow easier retrieval of all the data in `GetBackup`
+    const key_metaidx = "meta_idx";
+    var getMetaIndexes = function () {
+        return localStorageWrapper.getItem(PREFIX_DB + key_metaidx) || [];
+    };
+    var addMetaIndex = function (date) {
+        var idxs = getMetaIndexes();
+
+        if (idxs.indexOf(date) === -1) {
+            idxs.push(date);
+            localStorageWrapper.setItem(PREFIX_DB + key_metaidx, idxs);
+        }
+    };
+    var removeMetaIndex = function (date) {
+        var idxs = getMetaIndexes();
+
+        if (idxs.indexOf(date) !== -1) {
+            idxs.splice(date, 1);
+            localStorageWrapper.setItem(PREFIX_DB + key_metaidx, idxs);
+        }
+    };
+
     window.healthDatabase = {
         GetSettings: function () {
             return localStorageWrapper.getItem(PREFIX_DB + "settings");
@@ -47,6 +73,7 @@
         // date: timestamp (day, month and year only)
         SetItems: function (date, items) {
             if (typeof(items) !== undefined) {
+                addMetaIndex(date.toString());
                 localStorageWrapper.setItem(PREFIX_DB + "items_" + date.toString(), items);
             }
         },
@@ -68,6 +95,7 @@
                     },
                 });
                 this.SetItems(date, items);
+                addMetaIndex(date.toString());
             }
         },
         // date: timestamp (day, month and year only)
@@ -89,8 +117,32 @@
                 if (idx > -1) {
                     items.splice(idx, 1);
                     this.SetItems(date, items);
+                    if (!items.length) {
+                        removeMetaIndex(date.toString());
+                    }
                 }
             }
         },
+        GetBackup: function () {
+            var backup = {
+                meta: {
+                    prefix: PREFIX_DB,
+                    version: VERSION_DB,
+                },
+                settings: this.GetSettings(),
+                items: {},
+            };
+
+            for (var date of getMetaIndexes()) {
+                backup.items[date] = this.GetItems(date);
+            }
+
+            return JSON.stringify(backup);
+        },
+        RestoreBackup: function (backup) {
+            if (typeof(backup) !== undefined) {
+                backup = JSON.parse(backup);
+            }
+        }
     };
 })();
--- tags/backups.tag
+++ tags/backups.tag
@@ -9,7 +9,7 @@
                     <div class="panel-body">
                         <div class="row-fluid">
                             <div class="col-sm-3" style="text-align: center;">
-                                <button class="btn btn-primary"><i class="fa fa-2x fa-cloud-download"></i> save</button>
+                                <a id="downloadBackupButton" class="btn btn-primary" download="healthjs.bak"><i class="fa fa-2x fa-cloud-download"></i> save</a>
                             </div>
                             <div class="col-sm-8">
                                 Download a backup of all the items added to the application, as well as the settings
@@ -45,4 +45,16 @@
             </div>
         </div>
     </div>
+
+    this.DB = opts.DB
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/btoa
+    function utf8_to_b64(str) {
+        return window.btoa(unescape(encodeURIComponent(str)))
+    }
+    function b64_to_utf8(str) {
+        return decodeURIComponent(escape(window.atob(str)))
+    }
+
+    this.downloadBackupButton.setAttribute("href", "data:application/octet-stream;charset=utf-16le;base64," + utf8_to_b64(this.DB.GetBackup()))
 </backups>
